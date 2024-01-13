021e9b7746102318096691afb4b9900ef7eeb723
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2024-01-13 12:38:11 -0800

    added Error Handler Middleware Fixed a few issues

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import * as process from 'process';
 import { errors } from 'celebrate';
 import { requestLogger, errorLogger } from './middlewares/logger';
 import connectDb from './db/database';
 import userRouter from './routes/users';
 import sanitizeRequestBody from './middlewares/sanitizeRequestBody';
+import errorHandler from './middlewares/errorHandler';
 
 dotenv.config();
 const port = process.env.PORT ?? 3000;
 const app = express();
-
 app.use(express.json());
 app.use(sanitizeRequestBody);
 app.use(requestLogger);
@@ -23,6 +22,7 @@ app.get('/', (_: Request, res: Response) => {
 app.use('/users', userRouter);
 
 app.use(errorLogger);
+app.use(errorHandler);
 app.use(errors());
 
 app.listen(port, () => {
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,16 @@
-import {
-  ErrorRequestHandler, NextFunction, Request, Response,
-} from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { constants as errors } from 'http2';
+import * as process from 'process';
 
-function errorHandler<ErrorRequestHandler>(err: any, req: Request, res: Response, next: NextFunction) {
-  res.status(500);
-  next();
+function errorHandler(err: any, res: Response) {
+  const errorStatus = err?.status ?? errors.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+  const errorMessage = err.message ?? 'Произошла непредвиденная ошибка';
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: process.env.NODE_ENV === 'development' ? err?.stack : {},
+  });
 }
+
+export default errorHandler;
