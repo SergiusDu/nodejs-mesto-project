178ea4418081b825cbfc0955a0b38a56aa68bb1a
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2024-01-19 14:15:33 -0800

    - fixed a few issues with cards

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Response } from 'express';
 import { constants as errors } from 'http2';
 import * as process from 'process';
 
--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -13,7 +13,6 @@ cards.post('/', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().uri().required(),
-    owner: Joi.string().required(),
     createAt: Joi.date().required(),
   }),
 }), createCard);
@@ -25,10 +24,10 @@ cards.delete('/:cardId', celebrate(
     }),
   },
 ), deleteCard);
-cards.put('/cards/:cardId/likes', celebrate(
+cards.put('/:cardId/likes', celebrate(
   {
     [Segments.PARAMS]: Joi.object().keys({
-      cadId: Joi.string().required(),
+      cardId: Joi.string().required(),
     }),
     [Segments.BODY]: Joi.object().keys(
       {
@@ -39,10 +38,10 @@ cards.put('/cards/:cardId/likes', celebrate(
     ),
   },
 ), addLikeToCard);
-cards.delete('/cards/:cardId/likes', celebrate(
+cards.delete('/:cardId/likes', celebrate(
   {
     [Segments.PARAMS]: Joi.object().keys({
-      cadId: Joi.string().required(),
+      cardId: Joi.string().required(),
     }),
     [Segments.BODY]: Joi.object().keys(
       {
