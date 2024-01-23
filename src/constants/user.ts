2f383d770ad247cd5e32c061b51ce2b12d0e7ce1
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2024-01-23 14:29:59 -0800

    - added auth method - added jest tests - bug fixes

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,3 +1,6 @@
 export const USER_SIGNIN_ROUTE = '/signin';
 export const USER_SIGNUP_ROUTE = '/signup';
 export const USER_BASE_ROUTE = '/users';
+export const USER_DELETE_ROUTE = '/me/delete';
+
+export const USER_CHANGE_AVATAR_ROUTE = '/me/avatar';
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,18 +6,22 @@ export default (req: Request, res: Response, next: NextFunction) => {
   try {
     const cookies = req.headers.cookie;
     if (!cookies) {
-      return next(new Error('Необходимо авторизоваться'));
+      return next(new Error(`Не обнаружены куки ${req.url}`));
     }
     const cookieArray = cookies.split(';');
     const jwtToken = cookieArray.find((cookie) => cookie.startsWith('jwt='))?.split('jwt=')[1];
     if (!jwtToken) {
-      return next(new Error('Необходимо авторизоваться'));
+      return next(new Error(`Необходимо авторизоваться ${req.url}`));
     }
     const { payload } = jwt.verify(jwtToken, 'some-secret-key', { complete: true });
-    if (!isValidJwsUserSignature(payload)) return next(new Error('Необходимо авторизоваться'));
+    if (!isValidJwsUserSignature(payload)) {
+      console.error('Ошибка авторизации', req.url);
+      return next(new Error(`Необходимо авторизоваться ${req.url}`));
+    }
     req.user = payload;
     return next();
   } catch (e) {
+    console.error('Ошибка авторизации', req.url);
     return next(e);
   }
 };
