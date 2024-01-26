860d07b5da59f389dc12ddc4a05c5516877b3522
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2024-01-25 18:24:19 -0800

    Fixed all marked issues

--- src/server/utils/validation/auth.ts
+++ src/server/utils/validation/auth.ts
@@ -3,32 +3,35 @@ import { isEmail, isURL } from 'validator';
 import { URL_REGEXP } from '../../constants/common';
 import {
   USER_ABOUT_MAX_LENGTH,
-  USER_ABOUT_MIN_LENGTH, USER_EMAIL_MIN_LENGTH,
+  USER_ABOUT_MIN_LENGTH,
+  USER_EMAIL_MIN_LENGTH,
   USER_NAME_MAX_LENGTH,
-  USER_NAME_MIN_LENGTH, USER_PASSWORD_MIN_LENGTH,
+  USER_NAME_MIN_LENGTH,
+  USER_PASSWORD_MIN_LENGTH,
 } from '../../constants/user';
+import ValidationError from '../../errors/validation-error';
 
 export const VALIDATE_SIGNUP = celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().min(USER_EMAIL_MIN_LENGTH).email().custom((value) => {
       if (isEmail(value)) return value;
-      throw new Error('Некорректный email');
+      throw new ValidationError('Некорректный email');
     })
       .required(),
     password: Joi.string().min(USER_PASSWORD_MIN_LENGTH).required(),
-    name: Joi.string().min(USER_NAME_MIN_LENGTH).max(USER_NAME_MAX_LENGTH).required(),
-    about: Joi.string().min(USER_ABOUT_MIN_LENGTH).max(USER_ABOUT_MAX_LENGTH).required(),
+    name: Joi.string().min(USER_NAME_MIN_LENGTH).max(USER_NAME_MAX_LENGTH),
+    about: Joi.string().min(USER_ABOUT_MIN_LENGTH).max(USER_ABOUT_MAX_LENGTH),
     avatar: Joi.string().uri().custom((value) => {
       if (isURL(value) && URL_REGEXP.test(value)) return value;
-      throw new Error('Некорректный URL');
-    }).required(),
+      throw new ValidationError('Некорректный URL');
+    }),
   }),
 });
 export const VALIDATE_SIGNIN = celebrate({
   [Segments.BODY]: {
     email: Joi.string().min(USER_EMAIL_MIN_LENGTH).email().custom((value) => {
       if (isEmail(value)) return value;
-      throw new Error('Некорректный email');
+      throw new ValidationError('Некорректный email');
     })
       .required(),
     password: Joi.string().min(USER_PASSWORD_MIN_LENGTH).required(),
